81b93f802882ccb62f12ab36c3999aa64d79587c
Currents <currentsaisyah@gmail.com>
2026-06-06 10:16:30 +0700

    feat: implement room CRUD functionality

--- app.py
+++ app.py
@@ -76,3 +76,69 @@ def delete_tenant(id):
     db.commit() 
     
     return redirect('/tenant')
+
+# ROOM CRUD
+@app.route('/room')
+def room():
+    cursor.execute("SELECT * FROM m_room")
+    data = cursor.fetchall()
+
+    return render_template('room.html', rooms=data)
+
+@app.route('/add_room', methods=['GET', 'POST'])
+def add_room():
+
+    if request.method == 'POST':
+        room_number = request.form['room_number']
+        type = request.form['type']
+        price = request.form['price']
+        status = request.form['status']
+
+        sql = """
+        INSERT INTO m_room(room_number, type, price, status)
+        VALUES (%s,%s,%s,%s)
+        """
+
+        values = (room_number, type, price, status)
+
+        cursor.execute(sql, values)
+        db.commit()
+
+        return redirect('/room')
+
+    return render_template('add_room.html')
+
+@app.route('/edit_room/<int:id>', methods=['GET', 'POST'])
+def edit_room(id):
+
+    if request.method == 'POST':
+        room_number = request.form['room_number']
+        type = request.form['type']
+        price = request.form['price']
+        status = request.form['status']
+
+        sql = """
+        UPDATE m_room
+        SET room_number=%s, type=%s, price=%s, status=%s
+        WHERE room_id=%s
+        """
+
+        values = (room_number, type, price, status, id)
+
+        cursor.execute(sql, values)
+        db.commit()
+
+        return redirect('/room')
+
+    cursor.execute("SELECT * FROM m_room WHERE room_id=%s", (id,))
+    room = cursor.fetchone()
+
+    return render_template('edit_room.html', room=room)
+
+@app.route('/delete_room/<int:id>')
+def delete_room(id):
+
+    cursor.execute("DELETE FROM m_room WHERE room_id=%s", (id,))
+    db.commit()
+
+    return redirect('/room')
